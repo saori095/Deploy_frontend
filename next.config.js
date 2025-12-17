70be68d7b6a7c6ecfcd6b15683c4cbd74b8dee88
saori095 <sghdkakita@gmail.com>
2025-12-17 22:26:04 +0900

    commit9

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // ⇩の1行を追加
+    output: 'standalone',
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
